9804ae4dd7221b72b9b667c80826a9c847d88878
Nick Chapman <nic@amadido.co.uk>
2026-09-19 15:28:48 +0100

    timer interrupts

--- src/interrupt.s
+++ src/interrupt.s
@@ -4,6 +4,9 @@ PORTA = $6001
 DDRB  = $6002 ; data-direction register
 DDRA  = $6003
 
+T1CL  = $6004
+T1CH  = $6005
+
 ACR   = $600b ; auxillary control register (times and shift)
 PCR   = $600c ; peripheral control register (CA, CB)
 IFR   = $600d ; interrupt flag register
@@ -16,36 +19,42 @@ RS = %00100000 ; register select
 LCD_ClearDisplay               = %00000001
 LCD_ReturnHome                 = %00000010
 LCD_EntryMode_Inc_NoShift      = %00000110
+LCD_DisplayOn_CursorOff        = %00001100
 LCD_DisplayOn_CursorOn_NoBlink = %00001110
 LCD_DisplayShift_Left          = %00011000
 LCD_FunctionSet_8bit_2lines    = %00111000
 
+;;; zero page
+JIFFY = 20
+LAST_TICK = 21
+LAST_LED_TOGGLE = 22
+COUNTER = 23 ;; 3bytes in zero page
+
     org $8000
 
 via_init:
-    ;; set all pins on port-B as output
+    ;; set all pins on port-B and port-A as output
     lda #%11111111
     sta DDRB
-    ;; set most significant 3 pins on port-A as output
-    ;; and least sig for LED
-    lda #%11100001
     sta DDRA
-
     lda #%00000000 ; negative edge on any of CA1,CA2,CB1,CB2
     sta PCR
-
-    lda #%01111111 ; disable all
+    lda #%11000011 ; enable Timer1, CA1 and CA2
     sta IER
-    lda #%10000011 ; enable CA1 and CA2
-    sta IER
-    lda #$ff ; zero all interrupt flags
-    sta IFR
+    ;; Setup timer1
+    lda #%01000000 ; timer-1 in free running mode
+    sta ACR
+    ;; 4E1E = 19998 (100/s), at 2 MhZ
+    lda #$1e
+    sta T1CL
+    lda #$4e
+    sta T1CH ;; starts timer
     rts
 
 lcd_init:
     lda #LCD_FunctionSet_8bit_2lines
     jsr lcd_instruction
-    lda #LCD_DisplayOn_CursorOn_NoBlink
+    lda #LCD_DisplayOn_CursorOff
     jsr lcd_instruction
     lda #LCD_ClearDisplay
     jsr lcd_instruction
@@ -118,8 +127,6 @@ display_hex_byte: ; A->()
     jsr display_hex_nibble
     rts
 
-COUNTER = 20 ;; 3bytes in zero page
-
 init_counter:
     stz COUNTER
     stz COUNTER+1
@@ -149,11 +156,40 @@ display_counter:
 reset:
     jsr via_init
     jsr lcd_init
+    stz JIFFY
+    stz LAST_TICK
+    stz LAST_LED_TOGGLE
     jsr init_counter
     cli ; enable IRQ
 .loop:
     jsr display_counter
+    jsr tick_counter_every_second
+    jsr toggle_led_25ms
     jmp .loop
+
+tick_counter_every_second:
+    lda JIFFY
+    sec
+    sbc LAST_TICK
+    cmp #100
+    bne .done
+    lda JIFFY
+    sta LAST_TICK
+    jsr inc_counter
+.done:
+    rts
+
+toggle_led_25ms
+    lda JIFFY
+    sec
+    sbc LAST_LED_TOGGLE
+    cmp #50
+    bne .done
+    lda JIFFY
+    sta LAST_LED_TOGGLE
+    jsr toggle_led
+.done:
+    rts
 
 toggle_led:
     lda PORTA
@@ -161,33 +197,36 @@ toggle_led:
     sta PORTA
     rts
 
-nmi:
-    rti
-
 irq:
     pha
+    bit IFR
+    bvs .timer1
     lda IFR
     ror
     bcs .ca2
     ror
     bcs .ca1
-.hang:
     ;; IRQ fired; but IFR has 0s for ca1/2. how possible?
     ;; do nothing
     jmp .done
+.timer1:
+    inc JIFFY
+    lda #%01000000
+    sta IFR
+    jmp .done
 .ca1:
-    jsr toggle_led
-    inc COUNTER+2
+    inc COUNTER+1
     lda #%00000010
     sta IFR
     jmp .done
 .ca2:
-    inc COUNTER+1
+    inc COUNTER+2
     lda #%00000001
     sta IFR
     jmp .done
 .done:
     pla
+nmi: ;; silly share rti!
     rti
 
     org $fffa
