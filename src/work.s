4230b51f0b81967e3f0e442c16004d354a693e19
Nick Chapman <nic@amadido.co.uk>
2026-09-20 19:43:59 +0100

    share via setup between hello.s and work.s

--- src/work.s
+++ src/work.s
@@ -1,50 +1,7 @@
 
     org $8000
 
-PORTB = $6000
-PORTA = $6001
-DDRB  = $6002 ; data-direction register
-DDRA  = $6003
-
-T1CL  = $6004
-T1CH  = $6005
-
-ACR   = $600b ; auxillary control register (times and shift)
-PCR   = $600c ; peripheral control register (CA, CB)
-IFR   = $600d ; interrupt flag register
-IER   = $600e ; interrupt enable register
-
-E  = %10000000 ; enable
-RW = %01000000 ; read/write
-RS = %00100000 ; register select
-
-LCD_ClearDisplay               = %00000001
-LCD_ReturnHome                 = %00000010
-LCD_EntryMode_Inc_NoShift      = %00000110
-LCD_DisplayOn_CursorOff        = %00001100
-LCD_DisplayOn_CursorOn_NoBlink = %00001110
-LCD_DisplayShift_Left          = %00011000
-LCD_FunctionSet_8bit_2lines    = %00111000
-
-via_init:
-    ;; set all pins on port-B and port-A as output
-    lda #%11111111
-    sta DDRB
-    sta DDRA
-    lda #%00000000 ; negative edge on any of CA1,CA2,CB1,CB2
-    sta PCR
-    lda #%11000011 ; enable Timer1, CA1 and CA2
-    sta IER
-    ;; Setup timer1
-    lda #%01000000 ; timer-1 in free running mode
-    sta ACR
-    ;; 4E1E = 19998 (100/s), at 2 MhZ
-    lda #$1e
-    sta T1CL
-    lda #$4e
-    sta T1CH ;; starts timer
-    rts
-
+    include via.s
     include lcd.s
 
 display_hex_nibble: ; A->()
